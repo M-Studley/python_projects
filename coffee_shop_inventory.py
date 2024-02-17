5d5aab52097da2d3f55e566d1463904cb5bf4d5f
Michael Studley <michael.r.studley@gmail.com>
2024-02-17 17:09:08 +0700

    Updated coffee_shop_inventory
    added if statement for calculating time_in_store, changed __repr__ to loop through __dict__ instead of listing all the variables needed for creating an Item, changed class GetAddRemoveItem to ItemManager, changed get_items_in_catagory func to validate and loop, made add_category a private func, changed remove_item func to remove the item from the list in inventory instead of del, changed get_inventory to loop through the inventory dict to return all keys and values, added more clear testing below program.

--- coffee_shop_inventory.py
+++ coffee_shop_inventory.py
@@ -1,4 +1,9 @@
-inventory = {}
+inventory = {'coffee beans': [],
+             'dairy': [],
+             'dry goods': [],
+             'paper goods': [],
+             'ready to eat': [],
+             'water': []}
 
 
 class Item:
@@ -23,29 +28,32 @@ class Item:
         self.month_ordered = month_ordered
         self.checked_in = checked_in
         self.checked_out = checked_out
-        self.time_in_store: str = f"{self.checked_out - self.checked_in} days"
         self.cost_by_weight: str = f"{self.weight / self.price}/{self.unit_of_measurement}"
+        self.time_in_store: str
 
+        # calculates the time in store if both checked_in and checked_out have values
+        if (self.checked_in and self.checked_out) is not None:
+            self.time_in_store = f"{self.checked_out - self.checked_in} days"
+        elif self.checked_out is None:
+            self.time_in_store = "Item has not been checked out..."
+
+    # returns all information on an item
     def __repr__(self) -> str:
-        return (f"Name: {self.name}\nCategory: {self.category}\nQuantity: {self.quantity}\nPrice: {self.price}\n"
-                f"Weight: {self.weight}\nUnit of Measurement: {self.unit_of_measurement}\nPurveyor: {self.purveyor}\n"
-                f"Month Ordered: {self.month_ordered}\nChecked In: {self.checked_in}\nChecked Out: {self.checked_out}\n"
-                f"Time In Store: {self.time_in_store}\nCost By Weight: {self.cost_by_weight}")
+        return "\n".join([f"{key.capitalize()}: {value}" for key, value in self.__dict__.items()])
 
 
-class GetAddRemoveItem:
+class ItemManager:
     def __init__(self, item_name: Item):
         self.item = item_name
 
-    def get_item(self) -> str:
-        # returns the items name
-        return str(self.item.name)
-
     def get_items_in_category(self) -> str:
         # returns the category the item is listed in
-        return str(inventory[self.item.category])
+        if self.item.name in inventory[self.item.category]:
+            return "\n".join([item for item in inventory.get(self.item.category, [])])
+        else:
+            return f"'{self.item.name}' Not Found..."
 
-    def add_category(self) -> None:
+    def __add_category(self) -> None:
         # adds a category if non-existing.  used in the add_item function exclusively
         if self.item.category not in inventory:
             inventory[self.item.category] = []
@@ -54,7 +62,7 @@ class GetAddRemoveItem:
         # checks to see if the items category exists and adds if it does not or moves on if it does exist
         # will add the item to the inventory if non-existent.  prints status for confirmation
         if self.item.category not in inventory:
-            self.add_category()
+            self.__add_category()
 
         if self.item.name not in inventory[self.item.category]:
             inventory[self.item.category].append(self.item.name)
@@ -64,70 +72,139 @@ class GetAddRemoveItem:
 
     def remove_item(self):
         # will check for item in the category and produce a message
-        # if item in inventory{} it will delete it and print status message
+        # if item in inventory{} it will remove it and print a status message
         if self.item.name not in inventory[self.item.category]:
             print(f"'{self.item.name}' Not Found...")
         else:
-            item_location = inventory[self.item.category].index(self.item.name)
-            del inventory[self.item.category][item_location]
+            inventory[self.item.category].remove(self.item.name)
             print(f"'{self.item.name}' Removed Successfully...")
 
 
-lavazza_dark_roast = Item(
-    name='Lavazza Dark Roast',
-    category='Coffee Beans',
-    quantity=1,
-    price=1690.00,
-    weight=1000,
-    unit_of_measurement='g',
-    purveyor='Lazada',
-    month_ordered=2,
-    checked_in=47,
-    checked_out=60
-)
+def get_inventory() -> str:
+    # returns an inventory in string form
+    return "\n".join([f"{key.title()}: {value}" for key, value in inventory.items()])
 
-lavazza_medium_roast = Item(
-    name='Lavazza Medium Roast',
-    category='Coffee Beans',
-    quantity=1,
-    price=1590.00,
-    weight=1000,
-    unit_of_measurement='g',
-    purveyor='Lazada',
-    month_ordered=2,
-    checked_in=48,
-    checked_out=60
-)
 
-lavazza_light_roast = Item(
-    name='Lavazza Light Roast',
-    category='Coffee Beans',
-    quantity=1,
-    price=1490.00,
-    weight=1000,
-    unit_of_measurement='g',
-    purveyor='Lazada',
-    month_ordered=2,
-    checked_in=47,
-    checked_out=60
-)
+# # ADD_ITEM() TESTING and GET_INVENTORY() TESTING...
+# lavazza_dark_roast = Item(
+#     name='Lavazza Dark Roast',
+#     category='Coffee Beans',
+#     quantity=1,
+#     price=1690.00,
+#     weight=1000,
+#     unit_of_measurement='g',
+#     purveyor='Lazada',
+#     month_ordered=2,
+#     checked_in=47,
+#     checked_out=60)
+#
+# lavazza_light_roast = Item(
+#     name='Lavazza Light Roast',
+#     category='Coffee Beans',
+#     quantity=1,
+#     price=1490.00,
+#     weight=1000,
+#     unit_of_measurement='g',
+#     purveyor='Lazada',
+#     month_ordered=2,
+#     checked_in=47,
+#     checked_out=60)
+#
+# ItemManager(lavazza_dark_roast).add_item()
+# print(get_inventory())
+# print()
+# ItemManager(lavazza_light_roast).add_item()
+# print(get_inventory())
+# print()
 
-GetAddRemoveItem(lavazza_dark_roast).add_item()
-print(inventory)
-print(GetAddRemoveItem(lavazza_dark_roast).get_items_in_category())
-print()
-GetAddRemoveItem(lavazza_light_roast).add_item()
-print(inventory)
-print(GetAddRemoveItem(lavazza_light_roast).get_items_in_category())
-print()
-GetAddRemoveItem(lavazza_medium_roast).add_item()
-print(inventory)
-print(GetAddRemoveItem(lavazza_medium_roast).get_items_in_category())
-print()
-GetAddRemoveItem(lavazza_medium_roast).remove_item()
-print(inventory)
-print(GetAddRemoveItem(lavazza_medium_roast).get_items_in_category())
-print()
-GetAddRemoveItem(lavazza_medium_roast).remove_item()
-print(inventory)
-print(GetAddRemoveItem(lavazza_medium_roast).get_items_in_category())
+
+# # NO CHECKED_IN OR CHECKED_OUT TESTING
+# lavazza_breakfast_blend = Item(
+#     name='Lavazza Breakfast Blend',
+#     category='Coffee Beans',
+#     quantity=1,
+#     price=1390.00,
+#     weight=1000,
+#     unit_of_measurement='g',
+#     purveyor='Lazada',
+#     month_ordered=2)
+#
+# ItemManager(lavazza_breakfast_blend).add_item()
+# print(lavazza_breakfast_blend)
+# print()
+
+
+# # GET_ITEMS_IN_CATEGORY() TESTING...
+# lavazza_dark_roast = Item(
+#     name='Lavazza Dark Roast',
+#     category='coffee beans',
+#     quantity=1,
+#     price=1690.00,
+#     weight=1000,
+#     unit_of_measurement='g',
+#     purveyor='Lazada',
+#     month_ordered=2,
+#     checked_in=47,
+#     checked_out=60)
+#
+# lavazza_light_roast = Item(
+#     name='Lavazza Light Roast',
+#     category='coffee beans',
+#     quantity=1,
+#     price=1490.00,
+#     weight=1000,
+#     unit_of_measurement='g',
+#     purveyor='Lazada',
+#     month_ordered=2,
+#     checked_in=47,
+#     checked_out=60)
+#
+# # SUCCESSFUL
+# ItemManager(lavazza_dark_roast).add_item()
+# ItemManager(lavazza_light_roast).add_item()
+# print(get_inventory())
+# print(ItemManager(lavazza_dark_roast).get_items_in_category())
+# print()
+#
+# # UNSUCCESSFUL
+# ItemManager(lavazza_dark_roast).add_item()
+# print(get_inventory())
+# print(ItemManager(lavazza_light_roast).get_items_in_category())
+# print()
+
+
+# # REMOVE_ITEM() TESTING...
+# lavazza_dark_roast = Item(
+#     name='Lavazza Dark Roast',
+#     category='coffee beans',
+#     quantity=1,
+#     price=1690.00,
+#     weight=1000,
+#     unit_of_measurement='g',
+#     purveyor='Lazada',
+#     month_ordered=2,
+#     checked_in=47,
+#     checked_out=60)
+#
+# lavazza_light_roast = Item(
+#     name='Lavazza Light Roast',
+#     category='coffee beans',
+#     quantity=1,
+#     price=1490.00,
+#     weight=1000,
+#     unit_of_measurement='g',
+#     purveyor='Lazada',
+#     month_ordered=2,
+#     checked_in=47,
+#     checked_out=60)
+#
+# SUCCESSFUL
+# ItemManager(lavazza_dark_roast).add_item()
+# ItemManager(lavazza_light_roast).add_item()
+# ItemManager(lavazza_dark_roast).remove_item()
+# print(get_inventory())
+#
+# UNSUCCESSFUL
+# ItemManager(lavazza_dark_roast).add_item()
+# ItemManager(lavazza_light_roast).remove_item()
+# print(get_inventory())
