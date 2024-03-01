a35d4a4674fe6612e182de81986ebad4bfc4ad1c
Michael Studley <michael.r.studley@gmail.com>
2024-03-01 19:57:10 +0700

    Updated coffee_shop_inventory_test
    changing the tests to accommodate the updates from coffee_shop_inventory

--- inventory projects/coffee_shop_inventory_test.py
+++ inventory projects/coffee_shop_inventory_test.py
@@ -5,12 +5,23 @@ from coffee_shop_inventory import *
 test_item = Item(
     name='test item',
     category='test category',
+    purveyor='test purveyor',
     item_count=1,
     price=999.99,
     weight=99,
     unit_of_measurement='test unit',
-    purveyor='test purveyor',
-    month_ordered=13)
+    checked_in=40)
+name: str
+category: str
+purveyor: str
+item_count: int
+price: float
+weight: float
+unit_of_measurement: str
+checked_in: int
+checked_out: int
+time_in_store: str
+cost_by_weight: str
 
 # ItemManager.add_item(test_item)
 
@@ -19,176 +30,172 @@ test_item = Item(
 lavazza_dark_roast = Item(
     name='lavazza dark roast',
     category='coffee beans',
+    purveyor='lazada',
     item_count=1,
     price=1690.00,
     weight=1000,
     unit_of_measurement='g',
-    purveyor='lazada',
-    month_ordered=2,
-    checked_in=47,
-    checked_out=60)
+    checked_in=40)
 
 lavazza_medium_roast = Item(
     name='lavazza medium roast',
     category='coffee beans',
+    purveyor='lazada',
     item_count=1,
     price=1590.00,
     weight=1000,
     unit_of_measurement='g',
-    purveyor='lazada',
-    month_ordered=2)
+    checked_in=40)
 
 lavazza_light_roast = Item(
     name='lavazza light roast',
     category='coffee beans',
+    purveyor='lazada',
     item_count=1,
     price=1490.00,
     weight=1000,
     unit_of_measurement='g',
-    purveyor='lazada',
-    month_ordered=2,
-    checked_in=47,
-    checked_out=60)
+    checked_in=40)
 
-ItemManager.add_item(lavazza_dark_roast)
-ItemManager.add_item(lavazza_medium_roast)
-ItemManager.add_item(lavazza_light_roast)
+add_item(lavazza_dark_roast)
+add_item(lavazza_medium_roast)
+add_item(lavazza_light_roast)
 
 # ---------------- DAIRY ---------------- #
 
 heavy_cream = Item(
     name='heavy cream',
     category='dairy',
+    purveyor='makro',
     item_count=1,
     price=279.00,
     weight=1000,
     unit_of_measurement='ml',
-    purveyor='makro',
-    month_ordered=2)
+    checked_in=40)
 
 whole_milk = Item(
     name='milk (whole)',
     category='dairy',
+    purveyor='makro',
     item_count=1,
     price=95.00,
     weight=1000,
     unit_of_measurement='ml',
-    purveyor='makro',
-    month_ordered=2)
+    checked_in=40)
 
-ItemManager.add_item(heavy_cream)
-ItemManager.add_item(whole_milk)
+add_item(heavy_cream)
+add_item(whole_milk)
 
 # ---------------- DRY GOODS ---------------- #
 
 white_sugar = Item(
     name='sugar (white)',
     category='dry goods',
+    purveyor='shopee',
     item_count=1,
     price=38.00,
     weight=1000,
     unit_of_measurement='g',
-    purveyor='shopee',
-    month_ordered=2)
+    checked_in=40)
 
 brown_sugar = Item(
     name='sugar (brown)',
     category='dry goods',
+    purveyor='shopee',
     item_count=1,
     price=55.00,
     weight=1000,
     unit_of_measurement='g',
-    purveyor='shopee',
-    month_ordered=2)
+    checked_in=40)
 
-ItemManager.add_item(white_sugar)
-ItemManager.add_item(brown_sugar)
+add_item(white_sugar)
+add_item(brown_sugar)
 
 # ---------------- PAPER GOODS ---------------- #
 
 beverage_napkins = Item(
     name='beverage napkins (black)',
     category='paper goods',
+    purveyor='makro',
     item_count=1,
     price=59.00,
     weight=200,
     unit_of_measurement='piece',
-    purveyor='makro',
-    month_ordered=2)
+    checked_in=40)
 
 branded_napkins = Item(
     name='branded napkins (white)',
     category='paper goods',
+    purveyor='fancy napkin co.',
     item_count=1,
     price=120.00,
     weight=100,
     unit_of_measurement='piece',
-    purveyor='fancy napkin co.',
-    month_ordered=2)
+    checked_in=40)
 
-ItemManager.add_item(beverage_napkins)
-ItemManager.add_item(branded_napkins)
+add_item(beverage_napkins)
+add_item(branded_napkins)
 
 # ---------------- READY TO EAT ---------------- #
 
 ham_cheese_toastie = Item(
     name='ham & cheese toastie',
     category='ready to eat',
+    purveyor='makro',
     item_count=1,
     price=1560.00,
     weight=40,
     unit_of_measurement='piece',
-    purveyor='makro',
-    month_ordered=2)
+    checked_in=40)
 
 pumpkin_soup = Item(
     name='soup (pumpkin)',
     category='ready to eat',
+    purveyor='makro',
     item_count=1,
     price=1160.00,
     weight=3785,
     unit_of_measurement='ml',
-    purveyor='makro',
-    month_ordered=2)
+    checked_in=40)
 
 egg_tart = Item(
     name='tart (egg)',
     category='ready to eat',
+    purveyor='makro',
     item_count=1,
     price=960.00,
     weight=20,
     unit_of_measurement='piece',
-    purveyor='tops supermarket',
-    month_ordered=2)
+    checked_in=40)
 
-ItemManager.add_item(ham_cheese_toastie)
-ItemManager.add_item(pumpkin_soup)
-ItemManager.add_item(egg_tart)
+add_item(ham_cheese_toastie)
+add_item(pumpkin_soup)
+add_item(egg_tart)
 
 # ---------------- WATER ---------------- #
 
 singha_water_750ml = Item(
     name='singha bottled water (750ml)',
     category='water',
+    purveyor='makro',
     item_count=1,
     price=89.00,
     weight=12,
     unit_of_measurement='piece',
-    purveyor='big c',
-    month_ordered=2)
+    checked_in=40)
 
 singha_water_1000ml = Item(
     name='singha bottled water (1L)',
     category='water',
+    purveyor='makro',
     item_count=1,
     price=55.00,
     weight=6,
     unit_of_measurement='piece',
-    purveyor='big c',
-    month_ordered=2)
+    checked_in=40)
 
-ItemManager.add_item(singha_water_750ml)
-ItemManager.add_item(singha_water_1000ml)
+add_item(singha_water_750ml)
+add_item(singha_water_1000ml)
 
 
 '''-------------------------------- TESTS --------------------------------'''
@@ -212,7 +219,7 @@ ItemManager.add_item(singha_water_1000ml)
 # ItemManager.remove_item('lavazza dark roast')
 # print(full_inventory['lavazza dark roast'])
 # print()
-# print("\n" + InventoryManager.get_inventory())
+# print("\n" + get_inventory())
 #
 '''UNSUCCESSFUL'''
 # print()
@@ -228,14 +235,11 @@ ItemManager.add_item(singha_water_1000ml)
 '''CHANGE_CHECKED_IN TESTING...'''
 #
 # print()
-# print("singha 1000:", singha_water_1000ml)
+# print("singha 1000 (BEFORE):", full_inventory['singha bottled water (1L)'])
 # print()
-# print("full inventory:", full_inventory)
+# ItemManager._change_checked_in('singha bottled water (1L)', 49)
 # print()
-# ItemManager.change_checked_in('singha bottled water (1L)', 49)
-# print("singha 1000:", singha_water_1000ml)
-# print()
-# print("full inventory:", full_inventory)
+# print("singha 1000 (AFTER):", full_inventory['singha bottled water (1L)'])
 
 
 '''CHANGE_CHECKED_OUT TESTING...'''
@@ -326,9 +330,3 @@ ItemManager.add_item(singha_water_1000ml)
 #
 # print()
 # print(get_purveyor_sum('makro'))
-
-
-'''GET_ORDER_MONTH_SUM() TESTING...'''
-#
-# print()
-# print(get_order_month_sum(2))
